ebe70ff0db23b85768c285c9f9f74a39919fdf86
Broken Bones <micpiekar@lavabit.com>
2012-05-30 22:50:24 +0200

    Fixing last push.

--- OXGame.h
+++ OXGame.h
@@ -6,38 +6,109 @@
 #include <set>
 #include "boost/multi_array.hpp"
 
-//TODO michal
-//wciskaj wzorce projektowe i jakies sztuczki programistyczne gdzie tylko sie da bo jak nie tu to gdzie?^^
-class OXGame
-{
-public:
-	//TODO michal
-	OXGame(int size = 15);
-	//TODO michal
-	~OXGame();
-	//TODO michal
-	//startuje nowa gre	
-	void startNewGame();
-	//TODO michal
-	//wstawia obiekt field w dane miejsce, jesli w tym miejscu jest cos innego niz puste pole to wyjatek
-	//jesli po wstawieniu znajdzie sie 5 w lini to powiadamiasz action listenera	
-	//sprawdzanie zrob na wzorcu wizytatora zeby bylo ciekawiej
-	//jak brak mozliwosci ruchu to powiadomic tez
-	void put(Field &f , int x , int y) throw FieldTakenException;
+class OXGame {
+  public:
+    typedef boost::multi_array<Field*, 2> MapArray;
+    typedef std::set<EndOfGameListener *> ListenerSet;
+    OXGame(int size = 15);
+    ~OXGame();
+    void resetGame();
+    void put(Field &f, int x, int y);
+    void addEndOfGameListener(EndOfGameListener &l);
+    const Field& getField(int x, int y);
+    int getSize();
+  private:
+    class CheckIterator;
+    class HorizCheckIterator;
+    class VertCheckIterator;
+    class SlashCheckIterator;
+    class BackslashCheckIterator;
 
-	//TODO michal
-	void addEndOfGameListener( EndOfGameListener &l );
+    MapArray fields_;  
+    ListenerSet listeners_;
+    int size_;
+    int winningLineLength;
+    int fieldsTaken;
+    bool gameEnded;
 
-	//TODO michal
-	const Field& getField(int x , int y);
-
-	//TODO michal
-	int getSize();
-
-	private:
-	boost::multi_array<Field* , 2> fields_;	
-	//zbior wyeliminuje powtorzenia bo nie ma sensu kogos 2 razy powiadamiac
-	std::set<EndOfGameListener *> listeners_;
-	int size_; //obieramy size 15 ale zrob zeby dzialalo dla kazdego
+    void notifyEndOfGame(Field& f);
+    void endGame();
+    bool taken(const Field& f);
+    bool checkLine(CheckIterator& it);
+    HorizCheckIterator getHorizCheckIterator(int x, int y);
+    VertCheckIterator getVertCheckIterator(int x, int y);
+    SlashCheckIterator getSlashCheckIterator(int x, int y);
+    BackslashCheckIterator getBackslashCheckIterator(int x, int y);
+};
+class OXGame::CheckIterator {
+  public:
+    CheckIterator(const OXGame& ngame, int nx, int ny);
+    CheckIterator& operator++();
+    bool operator!=(CheckIterator& c);
+    virtual bool hasNext() = 0;
+  protected:
+    int curx, cury;
+    int size;
+    OXGame game;
+    int x, y;
+    int side;
+    char myType;
+  private:
+    virtual bool check() = 0;
+    virtual bool reverseCheck() = 0;
+    virtual void increment() = 0;
+    virtual void reverseIncrement() = 0;
+};
+class OXGame::HorizCheckIterator : public CheckIterator {
+  public:
+    bool hasNext();
+  private:
+    HorizCheckIterator(const OXGame& ngame, int x, int y) : 
+      CheckIterator(ngame, x, y) {}
+    friend OXGame::HorizCheckIterator 
+      OXGame::getHorizCheckIterator(int x, int y);
+    bool check();
+    bool reverseCheck();
+    void increment() { curx++; }
+    void reverseIncrement() { curx--; }
+};
+class OXGame::VertCheckIterator : public CheckIterator {
+  public:
+    bool hasNext();
+  private:
+    VertCheckIterator(const OXGame& ngame, int x, int y) : 
+      CheckIterator(ngame, x, y) {}
+    friend OXGame::VertCheckIterator 
+      OXGame::getVertCheckIterator(int x, int y);
+    bool check();
+    bool reverseCheck();
+    void increment() { cury++; }
+    void reverseIncrement() { cury--; }
+};
+class OXGame::SlashCheckIterator : public CheckIterator {
+  public:
+    bool hasNext();
+  private:
+    SlashCheckIterator(const OXGame& ngame, int x, int y) :
+      CheckIterator(ngame, x, y) {};
+    friend OXGame::SlashCheckIterator 
+      OXGame::getSlashCheckIterator(int x, int y);
+    bool check();
+    bool reverseCheck();
+    void increment() { curx++; cury++; }
+    void reverseIncrement() { curx--; cury--; }
+};
+class OXGame::BackslashCheckIterator : public CheckIterator {
+  public:
+    bool hasNext();
+  private:
+    BackslashCheckIterator(const OXGame& ngame, int x, int y) : 
+      CheckIterator(ngame, x, y) {};
+    friend OXGame::BackslashCheckIterator
+      OXGame::getBackslashCheckIterator(int x, int y);
+    bool check();
+    bool reverseCheck();
+    void increment() { curx--; cury++; }
+    void reverseIncrement() { curx++; cury--; }
 };
 #endif
--- fields.h
+++ fields.h
@@ -1,20 +1,43 @@
 #ifndef __Fieldsh__
 #define __Fieldsh__
 
-//absrakcyjna bazowa
-class Field
-{}; 
+class Field {
+  public:
+    virtual bool taken() const = 0;
+    //TODO tmp
+    virtual char type() const = 0;
+    //TODO tmp
+    virtual Field* copy() = 0;
+  private:
+    Field(const Field&);
+}; 
 
-//te trzy modyfikuj do woli tylko nazwy zostaw takie same bo bede wykorzystywane do rysowania w widoku
-class FieldX: public Field
-{};
+class FieldX: public Field {
+  public:
+    FieldX();
+    FieldX(const FieldX&);
+    bool taken() const { return true; }
+    char type() const { return 'x'; }
+    Field* copy() { return new FieldX(*this); }
+};
 
-class FieldO: public Field
-{};
+class FieldO: public Field {
+  public:
+    FieldO();
+    FieldO(const FieldO&);
+    bool taken() const { return true; }
+    char type() const { return 'o'; }
+    Field* copy() { return new FieldO(*this); }
+};
 
-class FieldEmpty: public Field
-{};
+class FieldEmpty: public Field {
+  public:
+    FieldEmpty();
+    FieldEmpty(const FieldEmpty&);
+    bool taken() const { return false; }
+    char type() const { return '0'; }
+    Field* copy() { return new FieldEmpty(*this); }
+};
 
-//nie wiem co tu bedzie potrzebne dodaj co uwazasz za sluszne
 class FieldTakenException {};
 #endif
