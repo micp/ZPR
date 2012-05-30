2e50faddf1d2c726de138e2405a9b875c9315937
Krzysiek Opasiak <ups100@tlen.pl>
2012-05-30 12:01:16 +0200

    Additional fields added

--- user.h
+++ user.h
@@ -18,16 +18,21 @@ public:
 
 	void setName(std::string name) { name_ = name; }
 
+	void setPoints( int points ) { points_ = points; }
+
+	int getPoints() { return points_; }
 	std::string& getName() { return name_; }
 
 	template<class Action>
 	void persist( Action &a )
 	{
 		Dbo::field(a , name_, "name");
+		Dbo::field(a , points_ , "points" );
 	}
 
 private:
 	std::string name_;
+	int points_;
 };
 
 typedef Auth::Dbo::AuthInfo<User> AuthInfo;
