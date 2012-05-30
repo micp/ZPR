bc92346d98872b70e1654f7b6e08c48427fa7ef8
Krzysiek Opasiak <ups100@tlen.pl>
2012-05-30 12:02:09 +0200

    Some bugs fixed, tests added

--- gamesconnector.h
+++ gamesconnector.h
@@ -3,46 +3,85 @@
 
 #include <boost/interprocess/sync/scoped_lock.hpp>
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
-#include <boost/shared_ptr>
+#include <boost/shared_ptr.hpp>
 
 #include <iterator>
+#include <set>
+#include <string>
 
-#include "game.h"
 #include "gamelistrefresher.h"
+#include "user.h"
+
+#ifdef __GAMESCONNECTOR_TEST__
+/**************************************************************************
+			Just for testing
+*************************************************************************/
+#include <boost/thread/thread.hpp>
+#include <boost/test/minimal.hpp>
+
+class Game
+{
+	public:
+	Game(User &user): name_(user.getName() ) {};
+	void join( GameListRefresher &ref) {}
+	const std::string& getServerUserName(){ return name_; }	
+	std::string name_; 
+};
+
+class Ref: public GameListRefresher
+{
+protected:
+	virtual	void refreshGameList() {}
+};
+ 
+#else
+//when normal compilation
+#include "game.h"
+#endif
 
 using namespace boost::interprocess;
 using namespace boost;
 using namespace std;
 
+//Exception class
 class IteratorNotValid {};
 
+//Class for connecting palyers into one game
 class GamesConnector
 {
 public:
+	struct Node
+	{
+		Node( Game *game , bool *valid ): game_( game ) , valid_( valid ) {}
+		boost::shared_ptr<Game> game_;
+		boost::shared_ptr<bool> valid_;
+	};
+
 	class const_iterator : public std::iterator<std::forward_iterator_tag, string >
 	{
-		friend class GameConnector;
+		friend class GamesConnector;
 	protected:
-		shared_ptr<bool> valid_;
+		boost::shared_ptr<bool> valid_;
 		set<Node>::iterator my_ele_;
 
-      		const_iterator( set<Node>::iterator my_ele ) : my_ele_( my_ele ) , valid_( my_ele->valid_ ) {}
+      		const_iterator( set<Node >::iterator my_ele ) :
+		 my_ele_( my_ele ) , valid_( my_ele->valid_ ) {}
 	
 	public:
 		const_iterator(){};
 
-		const_iterator( const_iterator& other): my_ele_( other.my_ele_ ) , valid( other.valid_ );
+		const_iterator( const const_iterator& other): my_ele_( other.my_ele_ ) , valid_( other.valid_ ) {}
 
 		const string& operator*() const
 		{
 			if( !*valid_ ) throw IteratorNotValid();
-		 	return my_ele_->game_.getServerUserName();	
+		 	return my_ele_->game_->getServerUserName();	
 	      	}
 
-	      	const T* operator->() const
+	      	const string* operator->() const
 	      	{
 			if( !*valid_ ) throw IteratorNotValid();
-		 	return &( my_ele_->game_.getServerUserName() );	
+		 	return &( my_ele_->game_->getServerUserName() );	
 	      	}
 
 	      	// pre
@@ -68,7 +107,7 @@ public:
 	      	const_iterator& operator--()
 	      	{
 		 	--my_ele_;
-			valid = my_ele_->valid_;
+			valid_ = my_ele_->valid_;
 		  	return *this;
 	      	}
 
@@ -76,7 +115,7 @@ public:
 	     	const_iterator operator--(int)
 	      	{
 		  	const_iterator tmp( *this );
-			--(*this)
+			--(*this);
 		  	return tmp;
 	      	}
 
@@ -92,13 +131,18 @@ public:
 		{ 	return *valid_; } 
 	};
 
-	GamesConnector& getInstance();
+	friend class const_iterator;
+
+	static GamesConnector& getInstance();
 
 	//means that user would like to set a new game
-	shared_ptr<Game> newGame( User &user );
+	boost::shared_ptr<Game> newGame( User &user );
 
 	//after succesfull return from this function you will be no longer notified about list changes
-	shared_ptr<Game> join( const_iterator &game , GameListRefresher &ref );
+	boost::shared_ptr<Game> join( const_iterator &game , GameListRefresher &ref );
+	
+	//call this if you are viewing a list and you'd like it to be allways actual
+	void refRegister( GameListRefresher &ref );
 	
 	//you should allways call this method when actual list game is not neccesary anymore for you
 	void unregister( GameListRefresher &ref );
@@ -114,20 +158,14 @@ public:
 	const_iterator begin() { return const_iterator( games_.begin() ); }
 
 	//DONOT use this unless between iterationBegin() and iterationEnd()
-	const_iterator end() { return const_iterator( games_.end(); }
-
-private:
-	struct Node
-	{
-		Node( Game *game , bool *valid ): game_( game ) , valid_( valid ) {}
-		shared_ptr<Game> game_;
-		shared_ptr<bool> valid_;
-	};
+	const_iterator end() { return const_iterator( games_.end() );  }
 	
+	//class which implements solution for readers and writters (writters prefered)
+	//Do not hesitate to use it if you have to
 	class Synchronizer
 	{
 	public:
-		Synchronizer(): readers_count( 0 ) , writters_count( 0 ) {}
+		Synchronizer(): readers_count_( 0 ) , writters_count_( 0 ) {}
 
 		void readerEnter();
 
@@ -138,20 +176,23 @@ private:
 		void writterLeave();
 	private:
 		interprocess_mutex mutex_reader_;
-		int readers_count;
+		int readers_count_;
 		interprocess_mutex mutex_writter_;
-		int writters_count;
+		int writters_count_;
 
 		interprocess_mutex lock_;
 		interprocess_mutex mutex_;
-		interprocess_mutex may_read__;
+		interprocess_mutex may_read_;
 	};	
+
+private:
 	
-	GamesConnector();
+	
+	GamesConnector(){}
 	
 	static GamesConnector *instance_;
 	static interprocess_mutex mutex_;
-	set<shared_ptr<Node> > games_;
+	set<Node> games_;
 	set<GameListRefresher *> refreshers_;
 	Synchronizer rw_synch_;
 };	
