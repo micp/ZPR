6ded0b121637b790979d67301011061cf0c3402b
Krzysiek Opasiak <ups100@tlen.pl>
2012-06-01 03:34:50 +0200

    Added some includes and constructor

--- endofgamelistener.h
+++ endofgamelistener.h
@@ -1,13 +1,22 @@
 #ifndef __EndOfGameListenerh__
 #define __EndOfGameListenerh__
 
+#include "game.h"
+#include "fields.h"
+
 //to ja zrobie pozniej bo to ja bede uzywal Ty tylko powadamiaj
 class EndOfGameListener
- {
+{
 public:
+	EndOfGameListener( Game *g): g_(g) {}
+	
 	void gameEnded(FieldEmpty f, int startX , int startY , int finishX , int finishY ); //gdy brak mozliwosci ruchu
+	
 	void gameEnded(FieldX f, int startX , int startY , int finishX , int finishY ); //gdy wygral X
+	
 	void gameEnded(FieldO f, int startX , int startY , int finishX , int finishY ); //gdy wygral Y
+private:
+	Game *g_;
 };
 
 #endif
