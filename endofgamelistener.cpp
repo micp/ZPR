65247fd424956585df97989c0dcc15ca36492795
Krzysiek Opasiak <ups100@tlen.pl>
2012-06-03 16:36:28 +0200

    Errors fixed

--- endofgamelistener.cpp
+++ endofgamelistener.cpp
@@ -8,8 +8,12 @@ void EndOfGameListener::gameEnded(FieldEmpty f, int startX , int startY , int fi
 	g_->startX_ = false;
 	g_->startO_ = false;
 	//TODO Co z punktami przy remisie?
-	g_->refX_->endedWithDraw(startX , startY , finishX , finishY);	
-	g_->refO_->endedWithDraw(startX , startY , finishX , finishY);	
+	WServer::instance()->post(g_->refX_->getSessionID() , boost::bind( &GameListRefresher::endedWithDraw,
+	g_->refX_ , startX , startY , finishX , finishY) ); 
+	WServer::instance()->post(g_->refO_->getSessionID() , boost::bind( &GameListRefresher::endedWithDraw,
+	g_->refO_ , startX , startY , finishX , finishY) ); 
+	//g_->refX_->endedWithDraw(startX , startY , finishX , finishY);	
+	//g_->refO_->endedWithDraw(startX , startY , finishX , finishY);	
 }
 
 void EndOfGameListener::gameEnded(FieldX f, int startX , int startY , int finishX , int finishY )
@@ -19,8 +23,12 @@ void EndOfGameListener::gameEnded(FieldX f, int startX , int startY , int finish
 	g_->startO_ = false;
 	//TODO
 	//odwolanie do bazy danych z update rankingu
-	g_->refX_->endedWithWin(startX, startY, finishX , finishY);
-	g_->refO_->endedWithLose(startX, startY, finishX , finishY);
+	WServer::instance()->post(g_->refX_->getSessionID() , boost::bind( &GameListRefresher::endedWithWin,
+	g_->refX_ , startX , startY , finishX , finishY) ); 
+	WServer::instance()->post(g_->refO_->getSessionID() , boost::bind( &GameListRefresher::endedWithLose,
+	g_->refO_ , startX , startY , finishX , finishY) ); 
+	//g_->refX_->endedWithWin(startX, startY, finishX , finishY);
+	//g_->refO_->endedWithLose(startX, startY, finishX , finishY);
 }
 
 void EndOfGameListener::gameEnded(FieldO f, int startX , int startY , int finishX , int finishY )
@@ -30,6 +38,10 @@ void EndOfGameListener::gameEnded(FieldO f, int startX , int startY , int finish
 	g_->startO_ = false;
 	//TODO
 	//odwolanie do bazy danych z update rankingu
-	g_->refO_->endedWithWin(startX, startY, finishX , finishY);
-	g_->refX_->endedWithLose(startX, startY, finishX , finishY);
+	WServer::instance()->post(g_->refX_->getSessionID() , boost::bind( &GameListRefresher::endedWithLose,
+	g_->refX_ , startX , startY , finishX , finishY) ); 
+	WServer::instance()->post(g_->refO_->getSessionID() , boost::bind( &GameListRefresher::endedWithWin,
+	g_->refO_ , startX , startY , finishX , finishY) ); 
+	//g_->refO_->endedWithWin(startX, startY, finishX , finishY);
+	//g_->refX_->endedWithLose(startX, startY, finishX , finishY);
 }
